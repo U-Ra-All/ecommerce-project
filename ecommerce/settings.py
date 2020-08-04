e040e6d788e677dd1a5f08d9b8003c56e89c5fb9
YouRa <allakhverdovyuriy@gmail.com>
2020-08-04 11:06:40 +0300

    Add Static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,4 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ecommerce/static/')
+]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
+
+
+
