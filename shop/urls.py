e1736df1d98fb0d79105e57e5752cf441f62b890
YouRa <allakhverdovyuriy@gmail.com>
2020-08-18 13:05:54 +0300

    Display Products On Homepage

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<slug:category_slug>', views.home, name='products_by_category'),
     path('product/', views.product, name='product'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 
 # Create your views here.
 
-def home(request):
-	return render(request, 'home.html')
+def home(request, category_slug=None):
+	category_page = None
+	products = None
+	if category_slug != None:
+		category_page = get_object_or_404(Category, slug=category_slug)
+		products = Product.objects.filter(category=category_page, available=True)
+	else:
+		products = Product.objects.all().filter(available=True)
+	return render(request, 'home.html', {'category':category_page, 'products': products})
 
 
 def product(request):
