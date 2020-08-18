645290b9e83728d288d6de4ae2786692402555b0
YouRa <allakhverdovyuriy@gmail.com>
2020-08-18 16:43:38 +0300

    Add Categories To Navbar

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -11,6 +12,11 @@ class Category(models.Model):
 		ordering = ('name', )
 		verbose_name = 'category'
 		verbose_name_plural = 'categories'
+
+
+	def get_url(self):
+		return reverse('products_by_category', args=[self.slug])
+
 
 	def __str__(self):
 		return self.name
