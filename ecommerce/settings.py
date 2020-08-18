6402f4a4775db1a3a79f7a63019a3c4691aafc26
YouRa <allakhverdovyuriy@gmail.com>
2020-08-18 11:19:27 +0300

    Add Category Product Models

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,6 +132,9 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
+
 
 
 
