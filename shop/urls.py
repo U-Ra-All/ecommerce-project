f221cc20a10d6bd8a86588dcab000f00bcce42e3
YouRa <allakhverdovyuriy@gmail.com>
2020-09-30 13:33:53 +0300

    Add Login

--- shop/urls.py
+++ shop/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
     path('cart/remove_product/<int:product_id>', views.cart_remove_product, name='cart_remove_product'),
     path('account/create/', views.signUpView, name='signup'),
+    path('account/login/', views.loginView, name='login'),
+    path('account/signout/', views.signoutView, name='signout'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,9 @@ from .models import Category, Product, Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import Group, User
 from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+
 
 
 # Create your views here.
@@ -98,6 +101,29 @@ def signUpView(request):
 	else:
 		form = SignUpForm()
 	return render(request, 'signup.html', {'form': form})
+
+
+def loginView(request):
+	if request.method == 'POST':
+		form = AuthenticationForm(data=request.POST)
+		if form.is_valid():
+			username = request.POST['username']
+			password = request.POST['password']
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				return redirect('signup')
+
+	else:
+		form = AuthenticationForm()
+	return render(request, 'login.html', {'form': form})
+
+
+def signoutView(request):
+	logout(request)
+	return redirect('login')
 
 
 
